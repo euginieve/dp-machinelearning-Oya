a157951114f918226468ab047b7cd48d866611c5
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-08 12:46:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🖥Кластеризация данных на основе таблиц в эксель-файлах')
 
 st.info("Это веб-приложение для кластеризации ваших данных, хранящихся в эксель-файлах")
+
+df = pd.read_excel(https://github.com/dataprofessor/data/blob/master/penguins_cleaned.xlsx)
+df
 
 
