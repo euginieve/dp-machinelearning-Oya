f32094b65847d4677e0239bbdbee32f9398ac742
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-08 19:04:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,8 @@ with st.expander('Кластеризация методом k-means++'):
         except Exception as e:
           st.write(f"Ошибка при кластеризации {e}")
           return None
-          
-      k_means_plus_plus(df, int(k_means_cluster_quan))
+      if k_means_cluster_quan: 
+        k_means_plus_plus(df, int(k_means_cluster_quan))
   else:
     st.write('Загрузите файл во вкладке "Данные для загрузки"')
     
