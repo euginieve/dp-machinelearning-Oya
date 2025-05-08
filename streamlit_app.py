177625a78f3b48c1c9ffd929beab45b99efaab09
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-08 18:31:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ with st.expander('Данные для загрузки'):
   unploaded_file = st.file_uploader(label="Загрузите свой файл")
 
   if unploaded_file:
-    df = pd.read_excel(unploaded_file)
+    df = pd.read_excel(unploaded_file, index_col=0)
     df
     
 with st.expander('Подготовка датасета'):
@@ -20,6 +20,8 @@ with st.expander('Подготовка датасета'):
     st.header("Введите параметры подготовки данных")
     col_numbers = ["В датасете нет колонки для индекса"] + [i for i in range (1,df.shape[1]+1)]
     col_index = st.selectbox("Выберите номер колонки, которую желаете сделать индексом", col_numbers)
+    # if col_index == "В датасете нет колонки для индекса":
+    #   col_index = 0
     col_index_numerical = st.selectbox("Выберите, вариант обработки пустых значений численных переменных", ("Удалять строки, содержащие пустые значения", 
                                                                            "Заменять пустые значения на среднее значение в колонке",
                                                                            "Заменять пустые значения на моду в колонке"
@@ -39,6 +41,7 @@ with st.expander('Кластеризация методом k-means++'):
       k_means_cluster_quan = st.text_input("Введите количество кластеров")
       if k_means_cluster_quan and not k_means_cluster_quan.isdigit():
         st.write("Количество должно быть числом")
+        
 
       def k_means_plus_plus(df, quan_of_clusters):
     """
