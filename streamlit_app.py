74412346485445876b881bc07d680ef92b6ad096
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-10 13:06:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ with st.expander('Данные для загрузки'):
 
   if unploaded_file:
     # col_numbers = ["В датасете нет колонки для индекса"] + [i for i in range (1,df.shape[1]+1)]
-    col_index_change = st.selectbox("Выберите номер колонки, которую желаете сделать индексом", ["В датасете нет колонки для индекса",
+    col_index_change = st.selectbox("Выберите вариант индексирования", ["В датасете нет колонки для индекса",
                                                                                          "Индексом датасета является первый столбец"])
     if col_index_change:
       if col_index_change == "В датасете нет колонки для индекса":
@@ -34,12 +34,7 @@ with st.expander('Данные для загрузки'):
 with st.expander('Подготовка датасета'):
   if unploaded_file:
     st.header("Введите параметры подготовки данных")
-    # col_numbers = ["В датасете нет колонки для индекса"] + [i for i in range (1,df.shape[1]+1)]
-    # col_index = st.selectbox("Выберите номер колонки, которую желаете сделать индексом", col_numbers)
-    # if col_index != "В датасете нет колонки для индекса":
-    #   col_titles = df.columns.values.tolist()
-    #   df.set_index[]
-    #   col_index = 0
+
     col_index_numerical = st.selectbox("Выберите, вариант обработки пустых значений численных переменных", ("Удалять строки, содержащие пустые значения", 
                                                                            "Заменять пустые значения на среднее значение в колонке",
                                                                            "Заменять пустые значения на моду в колонке"
@@ -55,6 +50,39 @@ with st.expander('Подготовка датасета'):
     st.write('Загрузите файл во вкладке "Данные для загрузки"')
 
 with st.expander('Кластеризация методом k-means++'):
+  elbow_method_need = st.selectbox("Требуется ли построить график локтя для лучшего проедставления о необходимом количестве кластеров?", ("Да", "Нет"))
+  if elbow_method_need == "Да":
+    def elbow_method(df, max_clusters_quan):
+    """функция, которая поможет пользователю выбрать оптимальное количество кластеров. На вход принимает только таблицу (дата-фрейм)
+    и максимальное количество кластеров, указанное пользователем"""
+    
+    try:
+        if max_clusters_quan > len(df):
+            print("Указанное максимальное количество кластеров превышает длину таблицы. Укажите меньшее количество.")
+            return None
+        if max_clusters_quan < 2:
+            print("Указанное максимальное число кластеров меньше двух и, следовательно, не имеет смысла. Уквжите число кластеров юольшее или равное двум.")
+            return None
+        if max_clusters_quan != int(max_clusters_quan):
+            print("Максимальное количество кластеров должно быть целым числом.")
+            return None
+            
+        ssd = []
+        scaler = StandardScaler()
+        scaled_df = scaler.fit_transform(df)
+        for quan_of_clusters in range(2, max_clusters_quan+1):
+            model = KMeans(n_clusters = quan_of_clusters, init = "k-means++")
+            model.fit(scaled_df)
+            ssd.append(model.inertia_)
+        
+        plt.plot(range(2, max_clusters_quan+1), ssd, "o--")
+        plt.title("График локтя")
+        return None
+    
+    except Exception as e:
+        pst.write(f"Ошибка при использовании метода: {e}")
+        return None
+      
   if unploaded_file:   
       k_means_cluster_quan = st.text_input("Введите количество кластеров")
       if k_means_cluster_quan and not k_means_cluster_quan.isdigit():
@@ -79,6 +107,7 @@ with st.expander('Кластеризация методом k-means++'):
         df
   else:
     st.write('Загрузите файл во вкладке "Данные для загрузки"')
+    
     
 
 
