0578bb3c383ba8c4018e4c42bb8757d4391ba6f1
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-11 17:25:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,7 +159,7 @@ with st.expander('Кластеризация методом k-means++'):
 with st.expander('Иерархическая кластеризация'):  
   if unploaded_file:
     if df.shape[0]>=3:
-      st.write("Я здеся!")
+      st.write("Я тута!")
 
       def hierarchy_dendrogram(df, level=31):
         scaler = MinMaxScaler()
