8b12542de883109735d44a889d97279a7892e506
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-18 20:00:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -364,7 +364,7 @@ with st.expander('Метод DBSCAN'):
         
         st.pyplot(fig)
         
-      eps_to_use = st.selectbox("Выберите число эпсилон", np.linspace(shortest_dist, longest_dist, len(points)).tolist())
+      eps_to_use = st.number_input("Выберите параметр эпсилон", min_value=shortest_distance, max_value=longest_distance, step=float)
 
       
           
