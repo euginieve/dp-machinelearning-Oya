83dba06e2aa2e6647c1fe3e54a235409301216d8
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-18 21:05:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -344,8 +344,8 @@ with st.expander('Метод DBSCAN'):
           outlier_percent_min_samples = []
           
           for n in range(1, min_samples_max_quan+1):
-              dbscan = DBSCAN(min_samples=n)
-              dbscan.fit(df)
+              dbscan = DBSCAN(min_samples=n, eps=0.0001)
+              dbscan.fit(scaled_df)
               # number_of_outliers_min_samples.append(np.sum(dbscan.labels_ == -1))
               percent_outliers = 100*np.sum(dbscan.labels_ == -1) / len(dbscan.labels_)
               outlier_percent_min_samples.append(percent_outliers)
