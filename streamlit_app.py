726caeed9b424e25d54d69793f307ee363a25147
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-20 23:38:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,17 +84,17 @@ with st.expander('Подготовка датасета', expanded=st.session_st
                           df_filled[column].fillna(mode_value[0], inplace=True)
           df = df_filled
           
-      columns_to_encode = []    
-      for column in df.columns:
-        if not pd.api.types.is_numeric_dtype(df[column]):
-          columns_to_encode.append(column)
+        columns_to_encode = []    
+        for column in df.columns:
+          if not pd.api.types.is_numeric_dtype(df[column]):
+            columns_to_encode.append(column)
 
-      if categorial_to_numerical == "OrdinalEncoder":
-        encoder = OrdinalEncoder().fit_transform(df)
-      else:
-        encoder = OneHotEncoder().fit_transform(df)
+        if categorial_to_numerical == "OrdinalEncoder":
+          encoder = OrdinalEncoder().fit_transform(df)
+        else:
+          encoder = OneHotEncoder().fit_transform(df)
 
-      df[columns_to_encode] = encoder.fit_transform(df[columns_to_encode])
+        df[columns_to_encode] = encoder.fit_transform(df[columns_to_encode])
       
       # if scaler_method != "Не производить нормализацию":
       #   if scaler_method == "Стандартизация (StandartScaler)":
