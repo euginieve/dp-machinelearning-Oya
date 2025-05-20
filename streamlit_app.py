55bd9c41fd8376a3b313672a7499ae03f5782469
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-20 20:54:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.info("Это веб-приложение для кластеризации в
 with st.expander('Импорт данных'):
 
   unploaded_file = st.file_uploader(label="Загрузите свой файл")
-  df
+  unploaded_file
   st.session_state.preparation_state = False
     
 with st.expander('Подготовка датасета'):
