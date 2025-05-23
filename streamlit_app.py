57118edd5cb197e6559a0055cde2f8d673bf4237
euginieve <113831184+euginieve@users.noreply.github.com>
2025-05-23 16:39:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,10 +66,10 @@ with st.expander('Импорт и предобработка данных'):
 
     
     if st.session_state.preparation_state_button_clicked:
-      if col_index_change == "В датасете нет колонки для индекса":
-        df = unploaded_file_df.copy()
-      else:
-        df = pd.read_excel(unploaded_file, index_col = 0)
+        if col_index_change == "В датасете нет колонки для индекса":
+          df = unploaded_file_df.copy()
+        else:
+          df = pd.read_excel(unploaded_file, index_col = 0)
 
         df.dropna(axis=1, how='all', inplace=True)
 
